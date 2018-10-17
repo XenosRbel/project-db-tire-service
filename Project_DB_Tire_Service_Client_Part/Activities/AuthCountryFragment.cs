c0b0fee8b69d9f22901c8feed18ddb86d2b685af
Pavel <delldd@yandex.ru>
2018-10-17 21:59:34 +0300

    Список стран

--- Project_DB_Tire_Service_Client_Part/Activities/AuthCountryFragment.cs
+++ Project_DB_Tire_Service_Client_Part/Activities/AuthCountryFragment.cs
@@ -11,7 +11,7 @@ using Android.Support.V7.Widget;
 using Android.Util;
 using Android.Views;
 using Android.Widget;
-using Project_DB_Tire_Service_Client_Part.Adapters;
+using Java.Util;
 
 namespace Project_DB_Tire_Service_Client_Part.Activities
 {
@@ -31,28 +31,35 @@ namespace Project_DB_Tire_Service_Client_Part.Activities
             var toolbar = _view.FindViewById<Android.Widget.Toolbar>(Resource.Id.toolbar_country);
             this.Activity.SetActionBar(toolbar);
             this.Activity.ActionBar.SetDisplayHomeAsUpEnabled(true);
-            //this.Activity.ActionBar.SetDisplayShowHomeEnabled(true);
+            this.Activity.ActionBar.SetDisplayShowHomeEnabled(true);
+            toolbar.NavigationOnClick += Toolbar_NavigationOnClick;
 
             var listOfCountry = _view.FindViewById<ListViewCompat>(Resource.Id.list_view_country);
-            var countryArray = Resources.GetStringArray(Resource.Array.countryCodes);
-            //countryArray = countryArray.Select(x => x.Replace(',', ' ')).ToArray();
+            string[] countryArray = GetCountryData();
 
             listOfCountry.Adapter = new ArrayAdapter<string>(_view.Context, Resource.Layout.item_country, countryArray);
 
             return _view;
         }
 
-        private static Dictionary<CountryData, CountryData> GetMapCountry(string[] countryArray)
+        private void Toolbar_NavigationOnClick(object sender, EventArgs e)
         {
-            var country = new Dictionary<CountryData, CountryData>();
 
-            for (int i = 0; i < countryArray.Length; i++)
+                this.FragmentManager.PopBackStack();
+
+        }
+
+        private string[] GetCountryData()
+        {
+            var countryMatrix = Resources.GetStringArray(Resource.Array.countryCodes).Select(x => x.Split(' ')).ToArray();
+            var countryArray = new string[countryMatrix.Length];
+
+            for (int i = 0; i < countryMatrix.Length; i++)
             {
-                var item = countryArray[i].Split(',');
-                country.Add(new CountryData(item[1]), new CountryData(item[0]));
+                countryArray[i] = ($"{new Locale("", countryMatrix[i][0]).DisplayCountry}\t{countryMatrix[i][1]}");
             }
 
-            return country;
+            return countryArray;
         }
     }
 }
